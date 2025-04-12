4b714d1bbac74106fcbef5911dd2c6883aee1b2e
Mykhailo Dudash <dudasmika17@gmail.com>
2025-04-12 18:49:16 +0200

    Dormitories CRUD functionality

--- app/src/main/java/com/example/db_mdudash/DBHelper.java
+++ app/src/main/java/com/example/db_mdudash/DBHelper.java
@@ -1,6 +1,8 @@
 package com.example.db_mdudash;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -39,8 +41,45 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS "+DBContract.Dormitories.TABLE_NAME);
         db.execSQL("DROP TABLE IF EXISTS "+DBContract.Rooms.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS "+DBContract.Dormitories.TABLE_NAME);
         onCreate(db);
     }
+
+    public long addDormitory(String name, String address){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(DBContract.Dormitories.COLUMN_ADDRESS, address);
+        values.put(DBContract.Dormitories.COLUMN_NAME, name);
+        long id = db.insert(DBContract.Dormitories.TABLE_NAME, null, values);
+        db.close();
+        return id;
+    }
+
+    public Cursor getAllDormitories(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        return db.query(DBContract.Dormitories.TABLE_NAME, null,null,null,null,null,null);
+    }
+
+    public int updateDormitory(int id, String name, String address){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(DBContract.Dormitories.COLUMN_NAME, name);
+        values.put(DBContract.Dormitories.COLUMN_ADDRESS, address);
+        String[] whereId = new String[]{String.valueOf(id)};
+        int rowsAffected = db.update(DBContract.Dormitories.TABLE_NAME, values, DBContract.Dormitories.COLUMN_ID + " = ?", whereId);
+        db.close();
+        return  rowsAffected;
+    }
+
+    public void deleteDormitory(int id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String[] whereId = new String[]{String.valueOf(id)};
+        db.delete(DBContract.Rooms.TABLE_NAME, DBContract.Rooms.COLUMN_DORMITORY_ID + " = ?", whereId);
+        db.delete(DBContract.Dormitories.TABLE_NAME, DBContract.Dormitories.COLUMN_ID + " = ?", whereId);
+        db.close();
+    }
+
+
+
 }
